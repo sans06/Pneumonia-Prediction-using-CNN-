584c22f367192204587a5e2bdd5606e74d8b2ddb
Sanskriti Kumar <55348806+sans06@users.noreply.github.com>
2021-04-23 20:32:00 +0530

    Add files via upload

---
+++ Flask App/Pneumonia_Prediction_Flask/app.py
@@ -0,0 +1,91 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import numpy as np
+from keras.preprocessing import image 
+
+
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+
+from keras.models import load_model
+from keras import backend
+
+
+import tensorflow as tf
+
+global graph
+tf.compat.v1.disable_eager_execution()
+graph=tf.compat.v1.get_default_graph()
+
+#global graph
+#graph = tf.get_default_graph()
+
+
+from skimage.transform import resize
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+
+model = tf.keras.models.load_model("models/mymodel1.h5")
+       # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        img = image.load_img(file_path, target_size=(64, 64))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+
+        with graph.as_default():
+            preds = model.predict_classes(x)
+        #index = ['No Tumor','Yes Tumor']
+        #text = "prediction : "+[preds[0]]
+            if preds[0][0]==0:
+                prediction="Normal"
+            else:
+                prediction="Pneumonia"
+        text = "prediction : "+prediction
+               # ImageNet Decode
+
+
+
+        return text
+
+
+
+if __name__ == '__main__':
+    app.run(debug=False,threaded = False)
